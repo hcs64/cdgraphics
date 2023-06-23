5dd460acd52146501d912ffd061382de47480c17
Adam Gashlin <agashlin@gmail.com>
2023-06-23 06:43:00 -0700

    Set up demo for VINL-1

--- demo/demo.js
+++ demo/demo.js
@@ -1,24 +1,26 @@
-const audioUrl = 'YOUR_MP3_FILE.mp3'
-const cdgUrl = 'YOUR_CDG_FILE.cdg'
+const cdgUrl = 'all.cdg'
 
 const CDGraphics = require('../index.js')
 const cdg = new CDGraphics()
 
 document.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app')
   const audio = document.getElementById('audio')
   const canvas = document.getElementById('canvas')
+  canvas.width = 300
+  canvas.height = 216
+
   const ctx = canvas.getContext('2d')
   let frameId
 
   const doRender = time => {
-    const frame = cdg.render(time, { forceKey: forceKeyCheckbox.checked })
+    const frame = cdg.render(time, { forceKey: false })
     if (!frame.isChanged) return
 
     createImageBitmap(frame.imageData)
       .then(bitmap => {
-        ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
-        ctx.imageSmoothingEnabled = false
-        ctx.drawImage(bitmap, 0, 0, canvas.clientWidth, canvas.clientHeight)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.drawImage(bitmap, 0, 0, canvas.width, canvas.height)
       })
   }
 
@@ -29,20 +31,36 @@ document.addEventListener('DOMContentLoaded', () => {
     doRender(audio.currentTime)
   }
 
-  // follow audio events (depending on your app, not all are strictly necessary)
-  audio.addEventListener('play', play)
-  audio.addEventListener('pause', pause)
-  audio.addEventListener('ended', pause)
-  audio.addEventListener('seeked', () => doRender(audio.currentTime))
+  // event handlers for track seek
+  const tracks = document.getElementById('track-list')
+  for (track of tracks.getElementsByClassName('track')) {
+    if (track.dataset && 'seek' in track.dataset) {
+      let [mm, ss, ff] = track.dataset.seek.split(":").map(n => parseInt(n, 10))
+      let seek_time = (mm * 60 + ss) + ff / 75
+      track.addEventListener('click', () => {
+        audio.currentTime = seek_time
+        audio.play()
+      })
+    }
+  }
 
   // download and load cdg file
   fetch(cdgUrl)
     .then(response => response.arrayBuffer())
     .then(buffer => {
+      app.classList.remove('loading-cdg')
       cdg.load(buffer)
-      audio.src = audioUrl // pre-load audio
-    })
 
-  // for demo UI only
-  const forceKeyCheckbox = document.getElementById('forceKey')
+      if (audio.isPaused) {
+        pause()
+      } else {
+        play()
+      }
+
+      // follow audio events (depending on your app, not all are strictly necessary)
+      audio.addEventListener('play', play)
+      audio.addEventListener('pause', pause)
+      audio.addEventListener('ended', pause)
+      audio.addEventListener('seeked', () => doRender(audio.currentTime))
+    })
 })
