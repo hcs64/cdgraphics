577d39971c3c2a403bc29710eb0a71d663a1d556
Adam Gashlin <agashlin@gmail.com>
2024-08-11 10:24:42 -0700

    source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, 'demo', 'demo.js'),
+  devtool: 'source-map',
   output: {
     path: path.join(__dirname, 'demo'),
     filename: 'bundle.js'
